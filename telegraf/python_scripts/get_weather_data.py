57873b4b96a4d528ea0c7498b6ee2ea735044b04
Michal Budik <Swiftblade1982@gmail.com>
2020-11-25 15:08:03 +0100

    Remove cpu temperature compensation from temperature collecting script on rpi
    Compensation for cpu temperature yielded wrong data, because rpi is now inside case.
    Because of that compensation is no longer needed.

--- telegraf/python_scripts/get_weather_data.py
+++ telegraf/python_scripts/get_weather_data.py
@@ -22,41 +22,11 @@ import traceback
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 
-def get_cpu_temperature() -> float:
-    """
-    Gets CPU temperature
-
-    :return: Float CPU temperature value
-    """
-    process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE, universal_newlines=True)
-    output, _error = process.communicate()
-    return float(output[output.index('=') + 1:output.rindex("'")])
-
-
-def get_compensated_temperature() -> float:
-    """
-    Temporary method compensating heat from CPU
-    
-    :return: Float compensated temperature value
-    """
-    comp_factor = 2.25
-    cpu_temp = get_cpu_temperature()
-    raw_temp = bme280.get_temperature()
-    comp_temp = raw_temp - ((cpu_temp - raw_temp) / comp_factor)
-    # print("""
-    # Compensated_Temperature: {:05.2f} *C
-    # Pressure: {:05.2f} hPa
-    # Relative humidity: {:05.2f} %
-    # """.format(temperature, pressure, humidity))
-    return comp_temp
-
-
 
 def main(argv):
     try:
         # TODO find out which temperature collection is more accurate raw/compensated
-        #temperature = bme280.get_temperature()
-        temperature = get_compensated_temperature()
+        temperature = bme280.get_temperature()
         pressure = bme280.get_pressure()
         humidity = bme280.get_humidity()
         print("sensor_temperature temperature={}".format(temperature))
