e396f3be1b509207ca31819a210b67301fcfb1e2
Michal Budik <Swiftblade1982@gmail.com>
2020-12-01 15:06:53 +0100

    Set Influxdb image version to 1.8.3
    Influxdb latest tag will be updated on version 2.0 in future which will bring a lot of changes. Setting tag to 1.8.3 will keep stable older version of Influxdb until I update my other files to work with Influxdb 2.0.

--- influxdb/run_influxdb.sh
+++ influxdb/run_influxdb.sh
@@ -1,3 +1,3 @@
 docker run --rm -d --name influxdb_database \
 -v $PWD/database_files:/var/lib/influxdb \
--p 8086:8086 influxdb
+-p 8086:8086 influxdb:1.8.3
--- influxdb/run_influxdb_client_compose.sh
+++ influxdb/run_influxdb_client_compose.sh
@@ -1,2 +1,2 @@
 docker run --rm -it --network=sensor_monitor_default --name influxdb_client \
---link=influxdb_database influxdb influx -host influxdb_database 
+--link=influxdb_database influxdb:1.8.3 influx -host influxdb_database 
