e4e74cbea6695105f2a95fa522977740cc5cf6eb
Michal Budik <swiftblade1982@mail.com>
2020-12-01 14:20:05 +0100

    Remove telegraf from docker compose
    Remove telegraf from docker compose because it will be reworked. Also i
    changed grafana user to 0 because grafana docker container needs to run
    with sudo privilages (root has id 0). This will need more research.

--- grafana/run_grafana.sh
+++ grafana/run_grafana.sh
@@ -1,4 +1,4 @@
-docker run --rm -d --name grafana --user 472 \
+docker run --rm --name grafana --user 0 \
 --volume "$PWD/grafana_data:/var/lib/grafana" \
 -p 3000:3000 \
 --link influxdb_database \
