d5c07a9858464613b249491c7b4882a1a797c7a3
Michal Budik <swiftblade1982@mail.com>
2020-12-04 12:13:27 +0100

    Rename influx db container name
    Rename influxdb container in shell scripts and docker compose to use
    widely used name influxdb.

--- influxdb/run_influxdb_config.sh
+++ influxdb/run_influxdb_config.sh
@@ -1,4 +1,4 @@
-docker run --rm -d --name influxdb_database \
+docker run --rm -d --name influxdb \
 -v $PWD/database_files:/var/lib/influxdb \
 -v $PWD/config_files/influxdb.conf:/etc/influxdb/influxdb.conf:ro \
 -p 8086:8086 \
