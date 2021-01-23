8d0d3d110eb528033294306729c646a1de3e0008
Michal Budik <swiftblade1982@gmail.com>
2021-01-23 19:01:52 +0100

    Rename nodemcu python data collection script

--- telegraf/nodemcu/run_telegraf_docker_test.sh
+++ telegraf/nodemcu/run_telegraf_docker_test.sh
@@ -1,4 +1,4 @@
 docker run --rm --name telegraf_test \
 -v $PWD/telegraf_docker_my.conf:/etc/telegraf/telegraf.conf:ro \
--v $PWD/get_weather_data_nodemcu.py:/etc/telegraf/get_weather_data_nodemcu.py \
+-v $PWD/get_sensor_data_nodemcu.py:/etc/telegraf/get_sensor_data_nodemcu.py \
 michbud98/telegraf-python:1.0 --test
