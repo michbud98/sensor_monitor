dd86c58d4e3ec70be3d39559b66225658951347e
Michal Budik <Swiftblade1982@gmail.com>
2021-01-24 20:48:09 +0100

    Create django app for main menu

--- django/control_web_app/control_web_app/settings.py
+++ django/control_web_app/control_web_app/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party
+
+    # own
+    "main_menu",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'control_web_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
