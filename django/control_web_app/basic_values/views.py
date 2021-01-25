b2abee8f70d0ef68ace92d60eeb6b8febf802091
Michal Budik <Swiftblade1982@gmail.com>
2021-01-25 19:13:00 +0100

    FIXUP Implementation of queries in Django apps

--- django/control_web_app/basic_values/views.py
+++ django/control_web_app/basic_values/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 
-from basic_values import querries
+from basic_values import queries
 
 # Create your views here.
 def basic_values_view(request, *args, **kwargs):
     my_context = {
-        "temperature_indoors" : querries.last_indoors_temperature,
-        "pressure_indoors" : querries.last_indoors_pressure,
-        "humidity_indoors" : querries.last_indoors_humidity,
-        "temperature_outdoors" : querries.last_outdoors_temperature,
-        "pressure_outdoors" : querries.last_outdoors_pressure,
-        "humidity_outdoors" : querries.last_outdoors_humidity
+        "temperature_indoors" : queries.last_indoors_temperature,
+        "pressure_indoors" : queries.last_indoors_pressure,
+        "humidity_indoors" : queries.last_indoors_humidity,
+        "temperature_outdoors" : queries.last_outdoors_temperature,
+        "pressure_outdoors" : queries.last_outdoors_pressure,
+        "humidity_outdoors" : queries.last_outdoors_humidity
 
     }
     return render(request, "basic_values.html", my_context)
--- django/control_web_app/room_control/views.py
+++ django/control_web_app/room_control/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
-from room_control import querries
+from room_control import queries
 
 # Create your views here.
 def sensor_list_view(request, *args, **kwargs):
     my_context = {
-        "sensor_id_list" : querries.query_all_tags,
+        "sensor_id_list" : queries.query_all_tags,
+    }
+    return render(request, "sensor_list.html", my_context)
+
     }
     return render(request, "sensor_list.html", my_context)
