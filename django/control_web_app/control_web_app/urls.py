79036cdaf0863360451659c3fe064344056a7898
Michal Budik <Swiftblade1982@gmail.com>
2021-01-26 21:18:00 +0100

    FIXUP Comment out code that I will work on later

--- django/control_web_app/control_web_app/urls.py
+++ django/control_web_app/control_web_app/urls.py
@@ -18,11 +18,11 @@ from django.urls import path
 
 from main_menu.views import home_view
 from basic_values.views import basic_values_view
-from room_control.views import sensor_list_view
+#from room_control.views import sensor_list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('basic_values/', basic_values_view, name='basic_values'),
-    path('sensor_list/', sensor_list_view, name='sensor_list'),
+    #path('sensor_list/', sensor_list_view, name='sensor_list'),
 ]
