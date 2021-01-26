5da8581f9b84d7a3ce3f534d0765c99bf19f1840
Michal Budik <Swiftblade1982@gmail.com>
2021-01-26 21:53:34 +0100

    Rework Dockerfile to run with postgress
    Django now runs in docker and it is connected to postgres database instead of sqlite, which cant be connected to Grafana.

--- django/control_web_app/control_web_app/settings.py
+++ django/control_web_app/control_web_app/settings.py
@@ -82,10 +82,16 @@ WSGI_APPLICATION = 'control_web_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# settings.py
+   
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
