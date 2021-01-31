77638fbb1ca840944882a6d5e4e099481ae465de
Michal Budik <Swiftblade1982@gmail.com>
2021-01-31 13:48:21 +0100

    Remove default value from sensor id room
    Change Room value in data model. It can now be unfilled.

--- django/control_web_app/room_control/models.py
+++ django/control_web_app/room_control/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Sensor(models.Model):
     sensor_id = models.CharField(max_length=30, unique=True)
-    hostname = models.CharField(max_length=30, null=True)
+    hostname = models.CharField(max_length=30, blank=True, null=True)
     location = models.CharField(max_length=30, default="nonset")
-    room = models.CharField(max_length=30, default="nonset")
+    room = models.CharField(max_length=30, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
