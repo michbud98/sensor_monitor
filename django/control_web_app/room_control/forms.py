be83ea1a183b39ca5fb922406143decb21a16aa9
Michal Budik <Swiftblade1982@gmail.com>
2021-02-01 17:30:46 +0100

    FIXUP Move label of room to labels dictionary in Sensor_form meta class
    Label was moved becaused overwrite that was used before prevented field from being blank.
    Label of location can't be moved this way because it is overwriten above meta class.

--- django/control_web_app/room_control/forms.py
+++ django/control_web_app/room_control/forms.py
@@ -5,7 +5,9 @@ from .models import Sensor
 class Sensor_form(forms.ModelForm):
     LOCATION_CHOICES = [("indoors", "Indoors"), ("outdoors", "Outdoors")]
     location = forms.CharField(label='Choose where sensor is located:', widget=forms.RadioSelect(choices=LOCATION_CHOICES))
-    room = forms.CharField(label='Choose room (if its outdoors leave blank):')
     class Meta:
         model = Sensor
         fields = "__all__"
+        labels = {
+            "room" : 'Choose room (if its outdoors leave blank):'
+        }
