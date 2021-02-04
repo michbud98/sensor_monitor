5c42d42c9352f7b7bf8c17f6951e74a5eb6e1587
Michal Budik <Swiftblade1982@gmail.com>
2021-02-04 15:02:18 +0100

    Create new model for rooms
    User will in create rooms in which he will place sensors and control elements.

--- django/control_web_app/room_control/views.py
+++ django/control_web_app/room_control/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 from sensor_control.models import Sensor
+from .models import Room
 
 # Create your views here.
 def room_list_view(request):
-
-    indoors_sensors_set = Sensor.objects.filter(location="indoors")
+    rooms_set = Room.objects.all()
     my_context = {
-        "indoors_sensors_set": indoors_sensors_set
+        "rooms_set": rooms_set
     }
     return render(request, "room_list.html", my_context)
+
+# def room_create_view(request):
+#     if request.method == "POST":
+#         form = Room_form(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect(room_list_view)
+#     else:
+#         form = Room_form()
+#         my_context ={
+#             'form':form,
+#         }
+#     return render(request, "sensor_create.html", my_context)
