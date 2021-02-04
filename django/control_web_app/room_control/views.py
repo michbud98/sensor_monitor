5496541ea8f6f931eea0bbb3b5344916982340fe
Michal Budik <Swiftblade1982@gmail.com>
2021-02-04 15:37:24 +0100

    Add room update view

--- django/control_web_app/room_control/views.py
+++ django/control_web_app/room_control/views.py
@@ -26,6 +26,20 @@ def room_create_view(request):
         }
     return render(request, "room_create.html", my_context)
 
+def room_update_view(request, room_id):
+    obj = get_object_or_404(Room, id=room_id)
+    form = Room_form(request.POST or None, instance=obj)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect(room_list_view)
+    if form.is_valid():
+        form.save()
+    context = {
+        'form': form
+    }
+    return render(request, "room_create.html", context)
+
 def room_detail_view(request, room_id):
     obj = get_object_or_404(Room, id=room_id)
     my_context = {
