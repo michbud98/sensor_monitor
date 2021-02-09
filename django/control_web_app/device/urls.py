c1c3a8f693b9fb68e214e3443169b43972d478f2
Michal Budik <Swiftblade1982@gmail.com>
2021-02-09 19:52:36 +0100

    Add view for requesting values of devices

--- django/control_web_app/device/urls.py
+++ django/control_web_app/device/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import (device_list_view, device_create_view, device_update_view,
- device_detail_view, device_values_edit_view, device_remove_view)
+ device_detail_view, device_values_edit_view, device_value_get, device_remove_view)
 
 urlpatterns = [
     # root path for this django app is [devices]
@@ -9,6 +9,7 @@ urlpatterns = [
     path('device_create', device_create_view, name='device_create'),
     path('<str:device_id>', device_detail_view, name='device_detail'),
     path('<str:device_id>/<int:room_id>/edit_values', device_values_edit_view, name='device_values_edit'),
+    path('<str:device_id>/get_value', device_value_get, name='device_values_edit'),
     path('<str:device_id>/update', device_update_view, name='device_update'),
     path('<str:device_id>/remove', device_remove_view, name='device_remove'),
 ]
--- django/control_web_app/device/views.py
+++ django/control_web_app/device/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
+from django.http import HttpResponse, JsonResponse
 
+from datetime import datetime
 from .models import Device, Thermo_head, Sunblind
 from .forms import Device_form, Thermo_head_form, Sunblind_form, Thermo_head_values_form, Sunblind_values_form
 
 # Create your views here.
+
+
 def device_list_view(request):
     devices_set = Device.objects.all()
     my_context = {
         "devices_set": devices_set
     }
     return render(request, "device_list.html", my_context)
+
 
 def device_create_view(request):
     if request.method == "POST":
@@ -25,10 +30,11 @@ def device_create_view(request):
             return redirect(device_list_view)
     else:
         form = Device_form()
-        my_context ={
-            'form':form,
+        my_context = {
+            'form': form,
         }
     return render(request, "device_create.html", my_context)
+
 
 def device_update_view(request, device_id):
     form = None
@@ -42,6 +48,7 @@ def device_update_view(request, device_id):
         'form': form
     }
     return render(request, "device_create.html", my_context)
+
 
 def device_detail_view(request, device_id):
     device = get_object_or_404(Device, device_id=device_id)
@@ -58,7 +65,7 @@ def device_detail_view(request, device_id):
         "sunblind": sunblind,
     }
     return render(request, "device_detail.html", my_context)
-    
+
 
 def device_values_edit_view(request, device_id, room_id):
     device = get_object_or_404(Device, device_id=device_id)
@@ -67,14 +74,16 @@ def device_values_edit_view(request, device_id, room_id):
     # REWORK Use device_types from model Devices
     if device.device_type == "thermo_head":
         thermo_head = get_object_or_404(Thermo_head, device_id=device_id)
-        initial_data = { "set_heat_value": thermo_head.set_heat_value }
-        form = Thermo_head_values_form(request.POST or None, initial=initial_data, instance=thermo_head)
+        initial_data = {"set_heat_value": thermo_head.set_heat_value}
+        form = Thermo_head_values_form(
+            request.POST or None, initial=initial_data, instance=thermo_head)
 
     elif device.device_type == "sunblind":
         sunblind = get_object_or_404(Sunblind, device_id=device_id)
-        initial_data = { "set_open_value": sunblind.set_open_value }
-        form = Sunblind_values_form(request.POST or None, initial=initial_data, instance=sunblind)
-    
+        initial_data = {"set_open_value": sunblind.set_open_value}
+        form = Sunblind_values_form(
+            request.POST or None, initial=initial_data, instance=sunblind)
+
     if request.method == "POST":
         if form.is_valid():
             form.save()
@@ -87,6 +96,28 @@ def device_values_edit_view(request, device_id, room_id):
         "room_id": room_id
     }
     return render(request, "device_values_edit.html", my_context)
+
+
+def device_value_get(request, device_id):
+    if request.method == "GET":
+        device = get_object_or_404(Device, device_id=device_id)
+        value = None
+        if device.device_type == "thermo_head":
+            specific_device = get_object_or_404(Thermo_head, device_id=device_id)
+            value = specific_device.set_heat_value
+            specific_device.last_requested_heat_value = value
+        elif device.device_type == "sunblind":
+            specific_device = get_object_or_404(Sunblind, device_id=device_id)
+            value = specific_device.set_open_value
+            specific_device.last_requested_open_value = value
+            
+        specific_device.last_request = datetime.today()
+        return_data = { "status_code": 200,"data": value }
+        return JsonResponse(return_data)
+    else:
+        return HttpResponse(status=405)
+    print()
+
 
 def device_remove_view(request, device_id):
     obj = get_object_or_404(Device, device_id=device_id)
