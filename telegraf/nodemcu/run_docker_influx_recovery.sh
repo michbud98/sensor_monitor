cee8cb7fbd2a2e96b1680fb61dd7b82675685300
michbud98 <swiftblade1982@gmail.com>
2021-03-02 18:47:30 +0100

    Change Influx docker image to official docker image

--- telegraf/nodemcu/run_docker_influx_recovery.sh
+++ telegraf/nodemcu/run_docker_influx_recovery.sh
@@ -1,1 +1,4 @@
-docker run --rm michbud98/telegraf-recovery
+docker run --rm \
+-v $PWD/influx_recovery_file_my.conf:/etc/telegraf/telegraf.conf:ro \
+-v $PWD/telegraf_logs:/tmp/ \
+michbud98/telegraf-recovery
