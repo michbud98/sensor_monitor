4a543f3b0f1945514a759a5da5af17db63ae1b06
michbud98 <swiftblade1982@gmail.com>
2021-03-05 17:44:26 +0100

    Add user registration

--- django/control_web_app/users/urls.py
+++ django/control_web_app/users/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from django.urls import path, include
 
+from .views import register
+
 app_name = 'users'
 urlpatterns = [
     # root path for this django app is [devices]
     path('', include('django.contrib.auth.urls')),
+    path('register/', register, name='register'),
+    # path('logout/', logout, name='logout')
 ]
--- django/control_web_app/users/views.py
+++ django/control_web_app/users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+from main_menu.views import home_view
+
+def register(request):
+    """Register a new user"""
+    if request.method != "POST":
+        # Display blank registration form
+        form = UserCreationForm()
+    else:
+        # Process completed form.
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Log the user in ant then redirect to homepage.
+            login(request, new_user)
+            return redirect(home_view)
+
+    # Display a blank or invalid form.
+    context = {'form': form }
+    return render(request, "registration/register.html", context)
+
+# def logout(request):
+#     logout(request)
+#     print("Logged out")
+#     return render(request, "registration/logged_out.html")
+
